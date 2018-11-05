23762b381312226ab10b6fca15c33059e16b9972
Raymond <raymond.50cal93@gmail.com>
2018-11-05 16:06:24 -0500

    First Commit

---
+++ src/test1/Main.java
@@ -0,0 +1,27 @@
+package test1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	   public static void main(String[] args) {
+	          launch(args);
+	   }
+
+	
+
+	
+	public void start(Stage primaryStage) throws Exception {
+		String fxmlResource = "Sample.fxml";
+	      Parent panel;
+	      panel = FXMLLoader.load(getClass().getResource(fxmlResource));
+	      Scene scene = new Scene(panel);
+	      Stage stage = new Stage();
+	      stage.setScene(scene);
+	      stage.show();
+		
+	}
+	}
